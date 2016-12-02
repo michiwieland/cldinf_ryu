650df5f227b4caafd94aaec86aea6b4d09daa2d0
Michael Wieland <michi.wieland@hotmail.com>
2016-12-02 17:13:51 +0100

    csv reader updated

--- controllers/csv_reader.py
+++ controllers/csv_reader.py
@@ -1,15 +1,22 @@
 import csv
+import os
 
 
 class CSVReader:
 
-    def __init__(self, fn='/opt/ryu/ryu/app/my/policies/policy.csv', delimiter=';'):
-        self.filename = fn
+    def __init__(self, filename='policies/policy.csv', delimiter=';'):
+        self.policies = dict()
         self.delimiter = delimiter
-        self.policies = {}
+        self.filename = os.path.join(os.path.dirname(__file__), filename)
+
+    def read():
         with open(self.filename, 'rb') as csvfile:
             reader = csv.reader(csvfile, delimiter=self.delimiter)
             for row in reader:
+                # use source host as key
                 source = row[0]
+                # use remaining hosts as valid destination values
                 destinations = row[1: len(row)]
                 self.policies[source] = destinations
+
+        return self.policies
